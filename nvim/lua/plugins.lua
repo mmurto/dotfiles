f8f58a42ed15b3e9c5302fcf4a019af4bce5c2c2
Mikko Murto <mikko.murto@hhpartners.fi>
2021-11-04 17:33:35 +0100

    move nvim plugin configs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -59,17 +59,7 @@ return require("packer").startup(
         }
 	use {
           "nvim-treesitter/nvim-treesitter",
-          config = function()
-            require('nvim-treesitter.configs').setup{
-              ensure_installed = "maintained",
-              highlight = {
-                enable = true
-              },
-              indent = {
-                enable = true
-              },
-            }
-          end
+          config = [[require('config.nvim-treesitter')]]
         }
         use 'SirVer/ultisnips'
         use "neovim/nvim-lspconfig"
@@ -79,68 +69,7 @@ return require("packer").startup(
         use 'hrsh7th/cmp-cmdline'
         use {
           'hrsh7th/nvim-cmp',
-          config = function()
-            local cmp = require'cmp'
-            cmp.setup({
-              snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                  -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                  -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                  vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                  -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
-                end,
-              },
-              mapping = {
-                ['<Tab>'] = function(fallback)
-                    if cmp.visible() then
-                      cmp.select_next_item()
-                    else
-                      fallback()
-                    end
-                  end,
-                  ['<S-Tab>'] = function(fallback)
-                    if cmp.visible() then
-                      cmp.select_prev_item()
-                    else
-                      fallback()
-                    end
-                  end,
-                ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-                ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-                ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                ['<C-e>'] = cmp.mapping({
-                  i = cmp.mapping.abort(),
-                  c = cmp.mapping.close(),
-                }),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-              },
-              sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                -- { name = 'vsnip' }, -- For vsnip users.
-                -- { name = 'luasnip' }, -- For luasnip users.
-                { name = 'path' },
-                { name = 'ultisnips' }, -- For ultisnips users.
-                -- { name = 'snippy' }, -- For snippy users.
-              })
-            })
-
-            -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline('/', {
-              sources = {
-                { name = 'buffer' }
-              }
-            })
-
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-              sources = cmp.config.sources({
-                { name = 'path' }
-              }, {
-                { name = 'cmdline' }
-              })
-            })
-          end
+          config = [[require('config.nvim-cmp')]]
         }
         use {
           "onsails/lspkind-nvim",
