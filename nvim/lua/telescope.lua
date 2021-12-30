55f59e78973635a38a41ce4a0a55eef57d3e030e
Mikko Murto <mikko.murto@hhpartners.fi>
2021-12-30 15:53:40 +0200

    neovim: use file picker if no git

--- nvim/lua/telescope.lua
+++ nvim/lua/telescope.lua
@@ -1,7 +1,14 @@
+
+project_files = function()
+  local opts = {} -- define here if you want to define something
+  local ok = pcall(require"telescope.builtin".git_files, opts)
+  if not ok then require"telescope.builtin".find_files(opts) end
+end
+
 vim.api.nvim_set_keymap(
     "n",
     "<C-p>",
-    ":Telescope git_files<CR>",
+    ":lua project_files()<CR>",
     {
         noremap = true,
         silent = true
