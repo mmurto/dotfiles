44de6fd1d6efff7003a09dcec9475a406e7182db
Mikko Murto <mikko.murto@hhpartners.fi>
2022-01-03 13:10:31 +0200

    neovim: map ö to : for visual mode

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -30,6 +30,7 @@ map("n", "K", [[<Cmd>lua vim.lsp.buf.hover()<CR>]], opt)
 map("n", "<C-k>", [[<Cmd>lua vim.lsp.buf.signature_help()<CR>]], opt)
 map("n", "<C-n>", ":NvimTreeToggle<CR>", opt)
 map("n", "ö", ":", opt)
+map("x", "ö", ":", opt)
 
 
 local t = function(str)
