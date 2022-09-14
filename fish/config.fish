435164c3bd75c9f565aaeb944e50f39bfd3ad412
Mikko Murto <mikko.murto@gmail.com>
2022-09-14 23:10:03 +0300

    cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/.local/bin $PATH
-set PATH /Users/mikko/Library/Python/2.7/bin $PATH
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 if type -q rvm:
 	rvm default
@@ -15,8 +14,3 @@ set -gx EDITOR nvim
 starship init fish | source
 
 zoxide init fish | source
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/mikko/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
